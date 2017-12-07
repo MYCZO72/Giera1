a6a8a0d88c0d4881a32500dc5e48ef587cbf526d
kiczak <szymonkot1@gmail.com>
2017-12-07 21:26:19 +0100

    skrypty pisze

---
+++ WavyGiera/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+	public int rows = 8;
+	public int columns = 8;
+
+	public GameObject darkTile;
+	public GameObject lightTile;
+	public Transform boardHolder;
+
+	public void generuj()
+	{
+		boardHolder = new GameObject("plansza").transform;
+		for(int i = 1; i <= columns; i++)
+		{
+			for(int j = 1; j <= rows; j++)
+			{
+				GameObject kratka;
+				if( ( i + j ) % 2 == 0 )
+					kratka = darkTile;
+				else
+					kratka = lightTile;
+
+				GameObject instance = Instantiate( kratka, new Vector3( i, j, 0f ), Quaternion.identity ) as GameObject;
+
+				instance.transform.SetParent( boardHolder );
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
