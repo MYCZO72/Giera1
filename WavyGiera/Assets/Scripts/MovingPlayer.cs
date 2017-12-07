0288d18450f3a8be0e72c7ca3f1586bb9fa1845a
kiczak <szymonkot1@gmail.com>
2017-12-07 21:50:21 +0100

    nietoperz spada

--- WavyGiera/Assets/Scripts/MovingPlayer.cs
+++ WavyGiera/Assets/Scripts/MovingPlayer.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlayer : MonoBehaviour {
+public class MovingPlayer : MonoBehaviour 
+{
+	public float speed;
+	Rigidbody2D gracz;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		gracz = GetComponent<Rigidbody2D>();	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate()
+	{
+		float movex = Input.GetAxis("Horizontal");
+		float movey = Input.GetAxis("Vertical");
+
+		Vector3 ruch = new Vector3( movex, movey, 0f );
+
+		gracz.AddForce( ruch * speed );
 	}
 }
